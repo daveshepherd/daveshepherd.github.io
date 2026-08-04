555645cd0b1db9df56a3c1dd0933f259af0be1f6
Dave Shepherd <daveshepherd@users.noreply.github.com>
2026-08-04 23:03:19 +0100

    chore(build): :green_heart: html proofer is blocked by stackoverflow, so excluding from checks (#106)

--- scripts/run-htmlproofer.sh
+++ scripts/run-htmlproofer.sh
@@ -8,6 +8,7 @@ readonly HTMLPROOFER_IGNORE_URLS=(
   "/https:\/\/medium\.com\/ww-engineering\/headers-propagation-with-hpropagate-27de8347f76a/"
   "/https:\/\/medium\.com\/@pierre\.meunier/"
   "/https:\/\/medium\.com\/r\/.*/"
+  "/https:\/\/stackoverflow\.com\/questions\/318804\/maven-or-ivy-for-managing-dependencies-from-ant/"
   "/https:\/\/github\.com\/daveshepherd"
 )
 
